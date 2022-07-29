f1ab29e3728c1418bae7529f401282072490fd08
jromatowski <joshuaromatowski@gmail.com>
2022-07-29 15:04:45 -0700

    changed seeds.js

--- db/seeds.js
+++ db/seeds.js
@@ -1,19 +1,21 @@
-const mongoose = require('./connection');
-const Bookmark = require('../models/Bookmark');
-const User = require('../models/User');
-const bookmarkSeeds = require('./seeds.json');
+//  As of now we don't need to delete the DB, but we're keeping the code for future reference in case.
 
-Bookmark.deleteMany({})
-	.then(() => User.deleteMany({}))
-	.then(() => {
-		return User.create({ email: 'fake@email.com', name: 'Fake Person' })
-			.then((user) =>
-				bookmarkSeeds.map((bookmark) => ({ ...bookmark, owner: user._id }))
-			)
-			.then((bookmarks) => Bookmark.insertMany(bookmarks));
-	})
-	.then(console.log)
-	.catch(console.error)
-	.finally(() => {
-		process.exit();
-	});
+// const mongoose = require('./connection');
+// const Bookmark = require('../models/Bookmark');
+// const User = require('../models/User');
+// const bookmarkSeeds = require('./seeds.json');
+
+// Bookmark.deleteMany({})
+// 	.then(() => User.deleteMany({}))
+// 	.then(() => {
+// 		return User.create({ email: 'fake@email.com', name: 'Fake Person' })
+// 			.then((user) =>
+// 				bookmarkSeeds.map((bookmark) => ({ ...bookmark, owner: user._id }))
+// 			)
+// 			.then((bookmarks) => Bookmark.insertMany(bookmarks));
+// 	})
+// 	.then(console.log)
+// 	.catch(console.error)
+// 	.finally(() => {
+// 		process.exit();
+// 	});
