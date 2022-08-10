51c7cc53bdd3ad7b31eb95b78c6acff35a53912f
jromatowski <joshuaromatowski@gmail.com>
2022-08-10 12:33:28 -0700

    updated Cart Schema

--- models/Cart.js
+++ models/Cart.js
@@ -3,12 +3,12 @@ const mongoose = require('../db/connection')
 const CartSchema = new mongoose.Schema({
 
 // Maybe turn "items" into schema
-	title: String,
+	// title: String,
 	// name: String,
 	items: {
 		// productId: Number,
 		name: String,
-		// imageUrl: String,
+		imageUrl: String,
 		price: Number,
 		quantity: Number,
 }
