ab76f694d84b3577aea4de7ce396a63c4878fd61
jromatowski <joshuaromatowski@gmail.com>
2022-08-09 17:10:49 -0700

    updated backend to fix quantity issue with Jay

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -7,7 +7,6 @@ router.get('/', async (req, res, next) => {
 	try {
 		const cartitems = await Cart.find({});
 		res.json(cartitems);
-		console.log(cartitems)
 	} catch (err) {
 		next(err);
 	}
@@ -23,25 +22,59 @@ router.get('/:id', async (req, res, next) => {
 	}
 });
 
+router.get('/:id/items/:quantity', async (req, res, next) => {
+	console.log(req.params)
+	try {
+		const cartitem = await Cart.findById(req.params.id);
+		res.json(cartitem);
+	} catch (err) {
+		next(err);
+	}
+});
+
 // Create: POST a Cart item
 router.post('/', async (req, res, next) => {
 	try {
 		// 1. Use the data in the req body to create a new ArtItem
 		const cart = await Cart.create(req.body);
+		// const newCart = await Cart.find({})
 		// 2. If the create is successful, send back the record that was inserted, specifying 201 status for Created
-		res.status(201).json(cart);
+		res.status(201).json(cart)
 	} catch (err) {
 		next(err);
 	}
 });
 
 // Update Cart Item by ID
-router.put('/:id', async (req, res, next) => {
+// router.put('/:id', async (req, res, next) => {
+//     try {
+//         const updatedCartItem = await Cart.findByIdAndUpdate(
+//             req.params.id,req.body, 
+//         {
+//             new: true}
+//         )
+//         if(updatedCartItem){
+//             res.json(updatedCartItem)
+//         } else {
+//             res.sendStatus(404)
+//         }
+//     } catch(err){
+//         next(err)
+//     }
+// })
+
+router.patch('/:id', async (req, res, next) => {
     try {
+		// console.log(req.params)
+		// console.log(req.params.quantity)
+		// console.log(req.params.id)
+		console.log(req.body)
         const updatedCartItem = await Cart.findByIdAndUpdate(
-            req.params.id,req.body, 
-        {
-            new: true}
+            req.params.id,
+			req.body, 
+    		{
+            new: true
+			}
         )
         if(updatedCartItem){
             res.json(updatedCartItem)
@@ -66,7 +99,9 @@ router.delete('/:id', async(req, res, next) => {
 // Route for deleting ALL
 router.delete('/', async(req, res, next) => {
 	try {
-		Cart.deleteMany({})		
+		await Cart.deleteMany({})
+		const cartitem = await Cart.find({});
+		res.json(cartitem);	
 	} catch(err) {
 		next(err)
 	}
